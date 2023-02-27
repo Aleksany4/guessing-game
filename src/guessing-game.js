547bffee8c16d7c0206df6d2d3a76d2f285643dc
Aleksany4 <aleksany4pl@gmail.com>
2023-02-27 22:38:15 +0100

    init: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.game = Math.round((this.min + this.max) / 2);
+        return this.game;
     }
 
     lower() {
-
+        this.max = this.game;
     }
 
     greater() {
-
+        this.min = this.game;
     }
 }
 
